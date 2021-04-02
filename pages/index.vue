4c97b238a474d71ce4f1d331d10d3ad874dacda4
Reyna Neet <reyna.neet@gmail.com>
2021-04-02 10:25:34 -0500

    Removed Nuxt logo, added name and personal github

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,12 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
       <h1 class="title">
-        website-2
+        Reyna Neet
       </h1>
       <div class="links">
         <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
+          href="https://github.com/reyna-neet"
           target="_blank"
           rel="noopener noreferrer"
           class="button--grey"
